6c92dd33064ded4e118705bfe5e2b6d649f969ab
r4m-t1n <im.r4mt1n@gmail.com>
2025-10-26 23:53:51 +0100

    refactor: update dataset class for Pexels API and balanced sampling

--- src/dataset.py
+++ src/dataset.py
@@ -1,75 +1,80 @@
-from typing import Union, List
+from typing import List
 import aiohttp
 import random
 import os
 import aiofiles
-from utils import download_batch
+from utils import download_batch_pexels
+from PIL import Image
+from io import BytesIO
 
 class Dataset:
     def __init__(
             self, labels: List[str],
-            batch_size: int = 16, num_samples: int = None,
-            transform = None, random_state: int = None,
-            min_shape: Union[List[int], tuple[int]] = None,
-            max_shape: Union[List[int], tuple[int]] = None,
-            min_size: int = None, max_size: int = None,
+            api_key: str,
+            num_samples: int,
+            transform=None, random_state: int = None,
             save_path: str = None):
 
         self.labels = labels
+        self.api_key = api_key
         self.num_samples = num_samples
-        self.batch_size = batch_size
+        self.transform = transform
+        self.save_path = save_path
 
         if random_state is not None:
             random.seed(random_state)
 
-        self.transform = transform
-        self.min_shape = min_shape
-        self.max_shape = max_shape
-        self.min_size = min_size
-        self.max_size = max_size
+        if self.num_samples % len(self.labels) != 0:
+            raise ValueError("num_samples should be divisible by the number of labels for a balanced dataset.")
 
-        self.labels_first_img = {l:0 for l in labels}
-        self.downloaded_count = 0
+        self.samples_per_label = self.num_samples // len(self.labels)
+
+        self.download_queue = []
+        for label in self.labels:
+            self.download_queue.extend([label] * self.samples_per_label)
+
+        random.shuffle(self.download_queue)
+
+        self.labels_page_number = {l: 1 for l in labels}
         self.seen_urls = set()
-
-        self.save_path = save_path
         if self.save_path:
             os.makedirs(self.save_path, exist_ok=True)
             self.save_counters = {label: 0 for label in self.labels}
 
-
     async def load_batch(self):
         async with aiohttp.ClientSession() as session:
-            while self.num_samples is None or self.downloaded_count < self.num_samples:
-                if self.num_samples is not None and self.downloaded_count >= self.num_samples:
-                    break
+            for label in self.download_queue:
+                image_downloaded = False
+                while not image_downloaded:
+                    async for image_data, url in download_batch_pexels(
+                            self.api_key, label, session,
+                            seen_urls=self.seen_urls,
+                            page=self.labels_page_number[label]
+                        ):
 
-                label = random.choice(self.labels)
-                async for image, first_img, url in download_batch(
-                        label, session,
-                        seen_urls=self.seen_urls,
-                        min_shape=self.min_shape, max_shape=self.max_shape,
-                        min_size=self.min_size, max_size=self.max_size,
-                        first_img=self.labels_first_img[label],
-                        batch_size=1
-                    ):
-                    
-                    self.labels_first_img[label] = first_img
-                    self.seen_urls.add(url)
+                        if image_data is None:
+                            break
 
-                    if self.save_path:
-                        file_counter = self.save_counters[label]
-                        file_name = f"{label}_{file_counter}.jpg"
-                        file_path = os.path.join(self.save_path, file_name)
-                        
-                        async with aiofiles.open(file_path, 'wb') as f:
-                            await f.write(image)
-                        
-                        self.save_counters[label] += 1
+                        self.seen_urls.add(url)
 
-                    if self.transform:
-                        image = self.transform(image)
-                    
-                    self.downloaded_count += 1
-                    yield image, label
-                    break
+                        if self.save_path:
+                            file_counter = self.save_counters[label]
+                            file_name = f"{label}_{file_counter}.jpg"
+                            file_path = os.path.join(self.save_path, file_name)
+
+                            async with aiofiles.open(file_path, 'wb') as f:
+                                await f.write(image_data)
+
+                            self.save_counters[label] += 1
+
+                        image_pil = Image.open(BytesIO(image_data)).convert("RGB")
+
+                        image_tensor = image_pil
+                        if self.transform:
+                            image_tensor = self.transform(image_pil)
+
+                        yield image_tensor, label
+                        image_downloaded = True
+                        break
+
+                    self.labels_page_number[label] += 1
